ff4bc55c1edce8dbdd82172df1747c699e7ef353
joaovsa97 <joaos.azevedo10@gmail.com>
2023-04-28 13:23:10 -0300

    create: register function(not working - CORS error)

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios"
 
 const Register = (props) => {
   const { isEditing, changeRegister } = props;
@@ -9,6 +10,21 @@ const Register = (props) => {
     email: "",
     password: "",
   });
+
+  const handleChange = (e) => {
+    setInputs(prev => ({ ...prev , [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      const res = await axios.post("http://localhost:8800/api/auth/register", Inputs)
+      console.log(res)
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div
       className="auth"
@@ -17,18 +33,18 @@ const Register = (props) => {
       <h1>Register</h1>
       <div className="form">
         <label htmlFor="username">Username</label>
-        <input type="text" placeholder="username" name="username" />
+        <input type="text" placeholder="username" name="username" onChange={handleChange}/>
         <label htmlFor="email">E-mail</label>
-        <input type="text" name="email" placeholder="E-mail" />
+        <input type="text" name="email" placeholder="E-mail" onChange={handleChange}/>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" placeholder="Password" />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange}/>
         <label htmlFor="confpswrd">Confirm Password</label>
         <input
           type="password"
           name="confpswrd"
           placeholder="Confirm Password"
         />
-        <button>Register</button>
+        <button onClick={handleSubmit}>Register</button>
         <span onClick={() => changeRegister()}>Login</span>
       </div>
     </div>
